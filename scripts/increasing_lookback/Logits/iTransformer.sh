5f5395318fa510b28ad5e274fddb03f6fc576d2e
benniebendiksen <pablobendiksen@gmail.com>
2025-03-24 16:30:03 -0400

    m2

--- scripts/increasing_lookback/Logits/iTransformer.sh
+++ scripts/increasing_lookback/Logits/iTransformer.sh
@@ -15,17 +15,16 @@ model_name=iTransformer
 
 
 #Fourth run
-#data_path="btcusdt_pca_components_12h_55_07_05.csv"
-#data_path="btcusdt_pca_components_55_window_1.csv"
-data_path="btcusdt_pca_components_12h_55_07_05_2.csv"
+data_path="btcusdt_pca_components_55_window_1.csv"
+#data_path="btcusdt_pca_components_12h_55_07_05_2.csv"
 seq_len=96
 pred_len=2
-enc_in=56
+enc_in=58
 d_model=512
 data_file=$(basename "$data_path" .csv)
 python -u run.py \
   --is_training 1 \
-  --root_path ./dataset/logits/ \
+  --root_path ./dataset/logits/pca_rolling_windows/ \
   --data_path $data_path \
   --model_id "1_${data_file}_${seq_len}_${pred_len}_${enc_in}" \
   --model $model_name \
@@ -51,15 +50,16 @@ python -u run.py \
   --precision_factor 2.0 \
   --auto_weight 1
 
-data_path="btcusdt_pca_components_12h_55_07_05_2.csv"
-seq_len=192
+  data_path="btcusdt_pca_components_55_window_2.csv"
+#data_path="btcusdt_pca_components_12h_55_07_05_2.csv"
+seq_len=96
 pred_len=2
-enc_in=56
+enc_in=58
 d_model=512
 data_file=$(basename "$data_path" .csv)
 python -u run.py \
   --is_training 1 \
-  --root_path ./dataset/logits/ \
+  --root_path ./dataset/logits/pca_rolling_windows/ \
   --data_path $data_path \
   --model_id "1_${data_file}_${seq_len}_${pred_len}_${enc_in}" \
   --model $model_name \
@@ -85,15 +85,16 @@ python -u run.py \
   --precision_factor 2.0 \
   --auto_weight 1
 
-data_path="btcusdt_pca_components_12h_55_07_05_2.csv"
-seq_len=48
+data_path="btcusdt_pca_components_55_window_3.csv"
+#data_path="btcusdt_pca_components_12h_55_07_05_2.csv"
+seq_len=96
 pred_len=2
-enc_in=56
+enc_in=58
 d_model=512
 data_file=$(basename "$data_path" .csv)
 python -u run.py \
   --is_training 1 \
-  --root_path ./dataset/logits/ \
+  --root_path ./dataset/logits/pca_rolling_windows/ \
   --data_path $data_path \
   --model_id "1_${data_file}_${seq_len}_${pred_len}_${enc_in}" \
   --model $model_name \
