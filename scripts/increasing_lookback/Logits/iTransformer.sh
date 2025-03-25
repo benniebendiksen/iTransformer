41f48916a87dd67ec305152b02a130d4ab91c1e8
benniebendiksen <pablobendiksen@gmail.com>
2025-03-24 21:10:26 -0400

    m2

--- scripts/increasing_lookback/Logits/iTransformer.sh
+++ scripts/increasing_lookback/Logits/iTransformer.sh
@@ -16,9 +16,43 @@ model_name=iTransformer
 
 #Fourth run
 # data_path="btcusdt_pca_components_55_window_1.csv"
-data_path="btcusdt_pca_components_12h_55_07_05_2.csv"
+data_path="btcusdt_pca_components_12h_55_09_03.csv"
 seq_len=96
 pred_len=1
+enc_in=58
+d_model=512
+data_file=$(basename "$data_path" .csv)
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/logits/ \
+  --data_path $data_path \
+  --model_id "1_${data_file}_${seq_len}_${pred_len}_${enc_in}" \
+  --model $model_name \
+  --data logits \
+  --features MS \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --e_layers 4 \
+  --enc_in $enc_in \
+  --dec_in $enc_in \
+  --c_out 1 \
+  --des 'Logits' \
+  --d_model $d_model \
+  --d_ff $d_model \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --itr 5 \
+  --train_epochs 50 \
+  --patience 7 \
+  --exp_name logits \
+  --target close \
+  --is_shorting 1 \
+  --precision_factor 2.0 \
+  --auto_weight 1
+
+data_path="btcusdt_pca_components_12h_55_09_03.csv"
+seq_len=96
+pred_len=2
 enc_in=58
 d_model=512
 data_file=$(basename "$data_path" .csv)
