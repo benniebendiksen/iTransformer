b5ab8352d05de28417de18c46d52598a9f26f0f9
benniebendiksen <pablobendiksen@gmail.com>
2025-04-03 22:17:04 -0400

    add datasets consolidated

--- scripts/increasing_lookback/Logits/iTransformer3.sh
+++ scripts/increasing_lookback/Logits/iTransformer3.sh
@@ -13,10 +13,47 @@ python -c "import torch; print('Torch CUDA Available:', torch.cuda.is_available(
 
 model_name=iTransformer
 
-data_path="btcusdt_pca_components_12h_1d_53_07_05.csv"
+# data_path="btcusdt_pca_components_12h_1d_53_07_05.csv"
+data_path="btcusdt_pca_components_12h_4h_65_07_05.csv"
 seq_len=96
 pred_len=1
-enc_in=58
+enc_in=70
+d_model=512
+data_file=$(basename "$data_path" .csv)
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/logits/ \
+  --data_path $data_path \
+  --model_id "1_${data_file}_${seq_len}_${pred_len}_${enc_in}" \
+  --model $model_name \
+  --data logits \
+  --features MS \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --e_layers 4 \
+  --enc_in $enc_in \
+  --dec_in $enc_in \
+  --c_out 1 \
+  --des 'Logits' \
+  --d_model $d_model \
+  --d_ff $d_model \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --itr 5 \
+  --train_epochs 50 \
+  --patience 7 \
+  --exp_name logits \
+  --target close \
+  --is_shorting 1 \
+  --precision_factor 2.0 \
+  --auto_weight 1 \
+  --freq 12h
+
+
+data_path="btcusdt_pca_components_12h_4h_65_07_05.csv"
+seq_len=96
+pred_len=1
+enc_in=66
 d_model=512
 data_file=$(basename "$data_path" .csv)
 python -u run.py \
