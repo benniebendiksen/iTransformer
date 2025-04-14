5a27b5d1fb87e75953af8e81c1e480fc0b83ec30
benniebendiksen <pablobendiksen@gmail.com>
2025-04-13 22:10:54 -0400

    new ds

--- scripts/increasing_lookback/Logits/iTransformer3.sh
+++ scripts/increasing_lookback/Logits/iTransformer3.sh
@@ -13,22 +13,49 @@ python -c "import torch; print('Torch CUDA Available:', torch.cuda.is_available(
 
 model_name=iTransformer
 
-exit 0
-
-
-btcusdt_pca_components_12h_4h_50_3672_282_no_l_juice_testing_22_test_cases.csv
-
-# Shifted, unchanged component number
-# data_path="btcusdt_pca_components_12h_4h_72_3572_282_shifted.csv"
-# data_path="btcusdt_pca_components_12h_4h_50_3672_282_no_l_juice_testing_22_test_cases.csv"
-# data_path="btcusdt_pca_components_12h_4h_70_3558_282_04_04_two.csv"
-#data_path="btcusd_pca_components_12h_4h_1d_80_7_5_bitsap.csv"
-#data_path="btcusd_pca_components_12h_reduced_4h_47_7_5_1_4_2_old.csv"
-
-data_path="btcusd_pca_components_xgboost_12h_4h_reduced_43_7_5_1_2_1_old.csv"
+# extended by 14 testing
+data_path="btcusd_pca_components_lightboost_12h_4h_reduced_extended_14_70_7_5_1_2_1_old.csv"
 seq_len=96
 pred_len=1
-enc_in=48
+#enc_in=65
+enc_in=75
+d_model=512
+data_file=$(basename "$data_path" .csv)
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/logits/ \
+  --data_path $data_path \
+  --model_id "${data_file}_${seq_len}_${pred_len}_${enc_in}" \
+  --model $model_name \
+  --data logits \
+  --features MS \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --e_layers 4 \
+  --enc_in $enc_in \
+  --dec_in $enc_in \
+  --c_out 1 \
+  --des 'Logits' \
+  --d_model $d_model \
+  --d_ff $d_model \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --itr 5 \
+  --train_epochs 50 \
+  --patience 7 \
+  --exp_name logits \
+  --target close \
+  --is_shorting 1 \
+  --precision_factor 2.0 \
+  --auto_weight 1 \
+  --freq 12h \
+
+  exit 0
+
+data_path="btcusd_pca_components_lightboost_12h_4h_reduced_60_7_5_1_2_1_old.csv"
+seq_len=96
+pred_len=1
+enc_in=61
 d_model=512
 data_file=$(basename "$data_path" .csv)
 python -u run.py \
@@ -60,11 +87,10 @@ python -u run.py \
   --auto_weight 1 \
   --freq 12h \
 
-
-#data_path="btcusd_pca_components_12h_reduced_4h_47_7_5_1_2_1_old.csv"
+#
 #seq_len=96
 #pred_len=1
-#enc_in=52
+#enc_in=45
 #d_model=512
 #data_file=$(basename "$data_path" .csv)
 #python -u run.py \
@@ -95,10 +121,12 @@ python -u run.py \
 #  --precision_factor 2.0 \
 #  --auto_weight 1 \
 #  --freq 12h \
+#  --dropout 0.15 \
+#
 #
 #seq_len=96
 #pred_len=1
-#enc_in=71
+#enc_in=45
 #d_model=512
 #data_file=$(basename "$data_path" .csv)
 #python -u run.py \
@@ -129,38 +157,4 @@ python -u run.py \
 #  --precision_factor 2.0 \
 #  --auto_weight 1 \
 #  --freq 12h \
-#
-## Shifted, adapted component number
-#seq_len=96
-#pred_len=1
-#enc_in=54
-#d_model=512
-#data_file=$(basename "$data_path" .csv)
-#python -u run.py \
-#  --is_training 1 \
-#  --root_path ./dataset/logits/ \
-#  --data_path $data_path \
-#  --model_id "1_${data_file}_${seq_len}_${pred_len}_${enc_in}" \
-#  --model $model_name \
-#  --data logits \
-#  --features MS \
-#  --seq_len $seq_len \
-#  --pred_len $pred_len \
-#  --e_layers 4 \
-#  --enc_in $enc_in \
-#  --dec_in $enc_in \
-#  --c_out 1 \
-#  --des 'Logits' \
-#  --d_model $d_model \
-#  --d_ff $d_model \
-#  --batch_size 32 \
-#  --learning_rate 0.001 \
-#  --itr 5 \
-#  --train_epochs 50 \
-#  --patience 7 \
-#  --exp_name logits \
-#  --target close \
-#  --is_shorting 1 \
-#  --precision_factor 2.0 \
-#  --auto_weight 1 \
-#  --freq 12h
+#  --dropout 0.2 \
