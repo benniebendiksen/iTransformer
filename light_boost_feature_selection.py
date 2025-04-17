62728ac29fb3578d987880b069b07ec83eadbc02
benniebendiksen <pablobendiksen@gmail.com>
2025-04-16 20:15:01 -0400

    new lance datasets 12 and 4

--- light_boost_feature_selection.py
+++ light_boost_feature_selection.py
@@ -62,9 +62,9 @@ def parse_args():
     #                     help='ratio of data to use for training')
     # parser.add_argument('--val_ratio', type=float, default=0.07,
     #                     help='ratio of data to use for validation')
-    parser.add_argument('--train_ratio', type=float, default=0.9,
+    parser.add_argument('--train_ratio', type=float, default=0.88,
                         help='ratio of data to use for training')
-    parser.add_argument('--val_ratio', type=float, default=0.1,
+    parser.add_argument('--val_ratio', type=float, default=0.07,
                         help='ratio of data to use for validation')
 
     # Optimization arguments
