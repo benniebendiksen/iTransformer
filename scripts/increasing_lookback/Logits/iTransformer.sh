e61766366a007c4e416fca8a2a7f598a7e5704a1
benniebendiksen <pablobendiksen@gmail.com>
2025-04-19 19:35:54 -0400

    x

--- scripts/increasing_lookback/Logits/iTransformer.sh
+++ scripts/increasing_lookback/Logits/iTransformer.sh
@@ -32,12 +32,12 @@ model_name=iTransformer
 #data_path="pca_components_btcusdt_12h_55_reduced_attempt_2.csv"
 #data_path="pca_components_btcusdt_12h_45_reduced_lance_seed.csv"
 #data_path="pca_components_btcusdt_12h_45_reduced_lance_seed_april_15.csv"
-data_path="pca_components_btcusdt_12h_44_reduced_lance_seed_april_15_baseline_set_sizes.csv"
-
+#data_path="pca_components_btcusdt_12h_44_reduced_lance_seed_april_15_baseline_set_sizes.csv"
+data_path="pca_components_btcusdt_12h_48_07_05_reduced_lance_seed_april_15.csv"
 seq_len=96
 pred_len=1
 # enc_in=73
-enc_in=49
+enc_in=53
 d_model=512
 data_file=$(basename "$data_path" .csv)
 python -u run.py \
