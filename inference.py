2e0a85174a463ed529c69c10cb1ae18763de134b
benniebendiksen <pablobendiksen@gmail.com>
2025-05-11 20:33:56 -0400

    begin iTrans regression for logits dataset

--- inference.py
+++ inference.py
@@ -2989,7 +2989,7 @@ def apply_conditional_trading_approach(preds, ffn_preds, trues, probs, ffn_probs
     }
 
 def apply_ensemble_approach(preds, ffn_preds, trues, probs, ffn_probs, actual_changes, is_shorting=True,
-                            confidence_threshold=0.7, ensemble_method='confidence_based'):
+                            confidence_threshold=0.7, ensemble_method='boosted'):
     """
     Apply an ensemble approach that combines both models based on their confidence
 
